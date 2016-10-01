6961142403d82cfc54c79a0a9b6903dd4aa02448
Gosuke Miyashita <gosukenator@gmail.com>
2016-10-01 17:40:07 +0900

    Pulling docker image should work now

--- main.go
+++ main.go
@@ -1,4 +1,38 @@
 package main
 
+import (
+	"flag"
+
+	log "github.com/Sirupsen/logrus"
+
+	"github.com/mobingilabs/go-modaemon/config"
+	"github.com/mobingilabs/go-modaemon/container/docker"
+	"github.com/mobingilabs/go-modaemon/server_config"
+)
+
 func main() {
+	const defaultModaemonConfig = "/opt/modaemon/modaemon.cfg"
+
+	var modaemonConfig string
+	flag.StringVar(&modaemonConfig, "c", defaultModaemonConfig, "path of modaemon.cfg")
+	flag.Parse()
+
+	c, err := config.LoadFromFile(modaemonConfig)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = serverConfig.Get(c.ServerConfigAPIEndPoint)
+
+	d, err := docker.New(s.Image, s.DockerHubUserName, s.DockerHubPassword)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = d.ImagePull()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
