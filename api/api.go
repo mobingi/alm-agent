8871da9f112c5be254f091ceadf656f731b6c5ad
Gosuke Miyashita <gosukenator@gmail.com>
2016-12-08 14:47:51 +0900

    Implement getting access token

--- api/api.go
+++ api/api.go
@@ -1,1 +1,50 @@
 package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+
+	"github.com/mobingilabs/go-modaemon/config"
+)
+
+type Client struct {
+	client    *http.Client
+	config    *config.Config
+	tokenType string
+	token     string
+}
+
+func NewClient(conf *config.Config) (*Client, error) {
+	c := &Client{
+		config: conf,
+		client: &http.Client{},
+	}
+
+	return c, nil
+}
+
+func (c *Client) getAccessToken() error {
+	values := url.Values{}
+	values.Set("grant_type", "client_credentials")
+	values.Set("client_id", c.config.StackID)
+	values.Set("client_secret", c.config.AuthorizationToken)
+
+	resp, err := c.client.PostForm(c.config.APIHost+"/v2/access_token", values)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	var res map[string]interface{}
+
+	if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return err
+	}
+
+	c.tokenType = res["token_type"].(string)
+	c.token = res["access_token"].(string)
+
+	return nil
+}
