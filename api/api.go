4d8d5148dabdf56ab3143f7e52c579fb202722b8
Gosuke Miyashita <gosukenator@gmail.com>
2016-12-09 09:47:45 +0900

    Implement getServerConfig()

--- api/api.go
+++ api/api.go
@@ -2,21 +2,28 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 
 	"github.com/mobingilabs/go-modaemon/config"
+	"github.com/mobingilabs/go-modaemon/server_config"
 )
 
-type Client struct {
+type conf struct {
+	ServerConfig *serverConfig.Config
+}
+
+type client struct {
 	client    *http.Client
 	config    *config.Config
 	tokenType string
 	token     string
 }
 
-func NewClient(conf *config.Config) (*Client, error) {
-	c := &Client{
+func NewClient(conf *config.Config) (*client, error) {
+	c := &client{
 		config: conf,
 		client: &http.Client{},
 	}
@@ -24,7 +31,41 @@ func NewClient(conf *config.Config) (*Client, error) {
 	return c, nil
 }
 
-func (c *Client) getAccessToken() error {
+func (c *client) getServerConfig() (*serverConfig.Config, error) {
+	values := url.Values{}
+	values.Set("stack_id", c.config.StackID)
+
+	res, err := c.get("/v2/alm/serverconfig", values)
+	if err != nil {
+		return nil, err
+	}
+
+	conf := &conf{}
+	err = json.Unmarshal(res, conf)
+	if err != nil {
+		return nil, err
+	}
+
+	return conf.ServerConfig, nil
+}
+
+func (c *client) get(path string, values url.Values) ([]byte, error) {
+	req, err := http.NewRequest("GET", c.config.APIHost+path, nil)
+	req.Header.Add("Authorization", fmt.Sprintf("%s %s", c.tokenType, c.token))
+
+	req.URL.RawQuery = values.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	res, err := ioutil.ReadAll(resp.Body)
+	return res, nil
+}
+
+func (c *client) getAccessToken() error {
 	values := url.Values{}
 	values.Set("grant_type", "client_credentials")
 	values.Set("client_id", c.config.StackID)
