a00678a0f9d4abd2cbb8027d392eabd37790f97d
sawanoboly <sawanoboriyu@higanworks.com>
2017-03-07 18:50:13 +0900

    add environmentVariables []string to Serverconfig

--- server_config/server_config.go
+++ server_config/server_config.go
@@ -1,11 +1,12 @@
 package serverConfig
 
 type Config struct {
-	Image             string
-	DockerHubUserName string
-	DockerHubPassword string
-	Code              string
-	CodeDir           string
-	GitReference      string
-	Ports             []int
+	Image                string
+	DockerHubUserName    string
+	DockerHubPassword    string
+	Code                 string
+	CodeDir              string
+	GitReference         string
+	Ports                []int
+	environmentVariables []string
 }
