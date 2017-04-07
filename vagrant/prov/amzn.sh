652000e079a3b945c75730f9b095cc0322f4acdb
sawanoboly <sawanoboriyu@higanworks.com>
2017-04-07 18:44:04 +0900

    install pkgs for build

--- vagrant/prov/amzn.sh
+++ vagrant/prov/amzn.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo 'export GOPATH=$HOME' >> ~/.bashrc
+yum install -y git gcc
+
+if [ ! -f $archive ]; then
+  wget -q https://storage.googleapis.com/golang/$archive
+fi
+
+if [ ! -d /home/ec2-user/go ]; then
+  tar -C /home/ec2-user -xzf $archive
+  chown -R ec2-user.ec2-user/home/ec2-user/go
+fi
+
+if ! grep GOPATH ~vagrant/.bashrc; then
+  echo 'export PATH=$PATH:$HOME/go/bin:$HOME/bin' >> /home/ec2-user/.bashrc
+  echo 'export GOPATH=$HOME' >> /home/ec2-user/.bashrc
+  echo 'sudo chown -R ec2-user.ec2-user $HOME/src' >> /home/ec2-user/.bashrc
+fi
