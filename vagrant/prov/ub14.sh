cceeb9d922e5db4acb57322602678763b0da1f60
sawanoboly <sawanoboriyu@higanworks.com>
2017-04-07 17:23:18 +0900

    export provisioner for ubuntu.

---
+++ vagrant/prov/ub14.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo Installing golang ...
+archive=go1.8.linux-amd64.tar.gz
+if [ ! -f $archive ]; then
+  wget -q https://storage.googleapis.com/golang/$archive
+fi
+
+if [ ! -d /usr/local/go ]; then
+  tar -C /usr/local -xzf $archive
+fi
+
+if ! grep GOPATH ~vagrant/.bashrc; then
+  echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/bin' >> ~vagrant/.bashrc
+  echo 'export GOPATH=$HOME' >> ~vagrant/.bashrc
+fi
+
+if [ ! -d ~vagrant/src ]; then
+  mkdir ~vagrant/src
+  chown vagrant:vagrant ~vagrant/src
+fi
+
+echo Installing pkgconf, git and cmake ...
+apt-get update
+apt-get install -y pkgconf git cmake
+
+mkdir -p ~vagrant/src/github.com/mobingilabs/go-modaemon
+chown -R vagrant:vagrant ~vagrant/src
+
+apt-get install -y apt-transport-https ca-certificates
+apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo deb https://apt.dockerproject.org/repo ubuntu-trusty main > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install -y docker-engine --force-yes
