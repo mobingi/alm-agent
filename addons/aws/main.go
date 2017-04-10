6c19e869840922141e7d83388927c7c4138841fb
sawanoboly <sawanoboriyu@higanworks.com>
2017-04-10 15:00:53 +0900

    WIP, trying autoscale info

--- addons/aws/main.go
+++ addons/aws/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/autoscaling"
 	"github.com/mobingilabs/go-modaemon/addons/aws/machine"
 	"github.com/mobingilabs/go-modaemon/api"
 	"github.com/mobingilabs/go-modaemon/config"
@@ -49,10 +50,48 @@ func main() {
 	}
 	log.Debugf("%#v", t)
 
-	sess, _ := session.NewSession(&aws.Config{
-		Region:      aws.String(instance.Region),
-		Credentials: credentials.NewStaticCredentials(t.AccessKeyID, t.SecretAccessKey, t.SessionToken),
-	})
+	cred := credentials.NewStaticCredentials(
+		t.AccessKeyID,
+		t.SecretAccessKey,
+		t.SessionToken,
+	)
+	awsconfig := aws.NewConfig().WithCredentials(cred).WithRegion(instance.Region)
 
+	if debug != "" {
+		awsconfig.WithLogLevel(aws.LogDebug)
+	}
+
+	sess := session.Must(session.NewSession(awsconfig))
+	// sess := session.Must(session.NewSession(aws.NewConfig().WithLogLevel(aws.LogDebug)))
 	log.Debugf("%#v", sess)
+
+	as := autoscaling.New(sess)
+	asparams := &autoscaling.DescribeAutoScalingInstancesInput{
+		InstanceIds: []*string{
+			aws.String(instance.InstanceID),
+		},
+	}
+
+	asresp, err := as.DescribeAutoScalingInstances(asparams)
+
+	if err != nil {
+		// Print the error, cast err to awserr.Error to get the Code and
+		// Message from an error.
+		log.Fatalf("%#v", err)
+		return
+	}
+
+	log.Debugf("%#v", asresp)
+	log.Infof("State: %s", *asresp.AutoScalingInstances[0].LifecycleState)
+
+	if *asresp.AutoScalingInstances[0].LifecycleState != "Terminating:Wait" {
+		return
+	}
+
+	log.Infof("Detected: Terminating:Wait")
+	// TODO: SPOT: deregister_instances_from_load_balancer => Machine
+	// TODO: cleanup crontab => Machine
+	// TODO: exec_shutdown_tasks_on_app_containers => Machine
+	// TODO: complete_lifecycle_action `CONTINUE``
+	// TODO: send_notification_to_api
 }
