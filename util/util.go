75615ef24a113169cd8affddc75015f336100dc0
sawanoboly <sawanoboriyu@higanworks.com>
2017-05-24 15:38:54 +0900

    create FetchContainerState func

--- util/util.go
+++ util/util.go
@@ -1,8 +1,28 @@
 package util
 
-import "os"
+import (
+	"io/ioutil"
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+)
 
 func FileExists(filename string) bool {
 	_, err := os.Stat(filename)
 	return err == nil
+}
+
+func FetchContainerState() string {
+	var container_status string = "/var/modaemon/containerlogs/log/container_status"
+	if !FileExists(container_status) {
+		return ""
+	}
+
+	dat, err := ioutil.ReadFile(container_status)
+	if err != nil {
+		return ""
+	}
+
+	log.Debugf("FetchContainerState: %s", string(dat))
+	return string(dat)
 }
