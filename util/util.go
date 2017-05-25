2ce322ebfdbcafe00ed06b258cc7093d2bea4103
sawanoboly <sawanoboriyu@higanworks.com>
2017-05-25 15:17:22 +0900

    add getServerIDforEC2 to util

--- util/util.go
+++ util/util.go
@@ -1,11 +1,15 @@
 package util
 
 import (
+	"errors"
 	"io/ioutil"
+	"net/http"
 	"os"
 
 	log "github.com/Sirupsen/logrus"
 )
+
+var METAENDPOINT = "http://169.254.169.254/"
 
 func FileExists(filename string) bool {
 	_, err := os.Stat(filename)
@@ -25,4 +29,36 @@ func FetchContainerState() string {
 
 	log.Debugf("FetchContainerState: %s", string(dat))
 	return string(dat)
+}
+
+func GetServerID(s ...string) (string, error) {
+	var sid string
+
+	if len(s) == 0 {
+		s[0] = "aws"
+	}
+
+	switch s[0] {
+	case "aws":
+		sid = getServerIDforEC2()
+	default:
+		return sid, errors.New("Provider is not supported.")
+	}
+
+	return sid, nil
+}
+
+func getServerIDforEC2() string {
+	resp, err := http.Get(METAENDPOINT + "/latest/meta-data/instance-id")
+	if err != nil {
+		log.Fatalf("%#v", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("%#v", err)
+	}
+	return string(body)
 }
