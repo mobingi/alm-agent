82959ca36f4d2a9cc7862e6a36c2f2931f284588
sawanoboly <sawanoboriyu@higanworks.com>
2017-08-02 18:56:32 +0900

    check pwd

--- .circleci/upload_to_s3.sh
+++ .circleci/upload_to_s3.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+AWSCLI="/usr/local/bin/aws"
+bucket="download.labs.mobingi.com"
+version=`cat ${CIRCLE_BRANCH}.json | jq -r .version`
+
+$AWSCLI s3 cp ${CIRCLE_BRANCH}.json s3://${bucket}/go-modaemon/
+$AWSCLI s3 cp bin/* s3://${bucket}/go-modaemon/${CIRCLE_BRANCH}/${version}/
+$AWSCLI s3 cp bin/* s3://${bucket}/go-modaemon/${CIRCLE_BRANCH}/current/
