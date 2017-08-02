3fa0fc3e61e48f60105685271039c6b99b10935a
sawanoboly <sawanoboriyu@higanworks.com>
2017-08-02 20:27:13 +0900

    upload per files

--- .circleci/upload_to_s3.sh
+++ .circleci/upload_to_s3.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-INFO_FILE="./bin/version_info.json"
+INFO_FILE="version_info.json"
+INFO_PATH="./bin/${INFO_FILE}"
 
-if ! [ -f ${INFO_FILE} ] ; then
-  echo 'JSONfile not found.'
+if ! [ -f ${INFO_PATH} ] ; then
+  echo 'INFO_FILE not found.'
   exit 1
 fi
 
 AWSCLI="/usr/local/bin/aws"
 bucket="download.labs.mobingi.com"
-version=`cat ${INFO_FILE} | jq -r .version`
+version=`cat ${INFO_PATH} | jq -r .version`
 
 mv bin ${version}
 tar cvzf ${version}/go-modaemon.tgz ${version}/*
-$AWSCLI s3 cp --recursive ${version}/ s3://${bucket}/go-modaemon/${CIRCLE_BRANCH}/${version}/
-$AWSCLI s3 cp --recursive ${version}/ s3://${bucket}/go-modaemon/${CIRCLE_BRANCH}/current/
+
+# upload guess per file
+for x in go-modaemon.tgz $INFO_FILE ; do
+  $AWSCLI s3 cp ${version}/${x} s3://${bucket}/go-modaemon/${CIRCLE_BRANCH}/${version}/
+  $AWSCLI s3 cp ${version}/${x} s3://${bucket}/go-modaemon/${CIRCLE_BRANCH}/current/
+done
