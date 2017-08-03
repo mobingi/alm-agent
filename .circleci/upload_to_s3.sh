cdf13ffe35ec4f62ebf7c608124a996269fb663f
sawanoboly <sawanoboriyu@higanworks.com>
2017-08-03 19:40:38 +0900

    json 3min

--- .circleci/upload_to_s3.sh
+++ .circleci/upload_to_s3.sh
@@ -19,4 +19,4 @@ $AWSCLI s3 cp ${version}/go-modaemon.tgz s3://${bucket}/go-modaemon/${CIRCLE_BRA
 $AWSCLI s3 cp --cache-control 'max-age=3600' ${version}/go-modaemon.tgz s3://${bucket}/go-modaemon/${CIRCLE_BRANCH}/current/
 
 $AWSCLI s3 cp --content-type text/json ${version}/${INFO_FILE} s3://${bucket}/go-modaemon/${CIRCLE_BRANCH}/${version}/
-$AWSCLI s3 cp --content-type text/json --cache-control 'max-age=600' ${version}/${INFO_FILE} s3://${bucket}/go-modaemon/${CIRCLE_BRANCH}/current/
+$AWSCLI s3 cp --content-type text/json --cache-control 'max-age=180' ${version}/${INFO_FILE} s3://${bucket}/go-modaemon/${CIRCLE_BRANCH}/current/
