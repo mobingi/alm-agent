d0dc1b6b95d11c805ad504b097949a4ae1cae933
sawanoboly <sawanoboriyu@higanworks.com>
2017-08-23 17:30:59 +0900

    TestGetBase, TestPostBase

--- api/api_test.go
+++ api/api_test.go
@@ -1,6 +1,10 @@
 package api
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/mobingi/alm-agent/config"
@@ -30,5 +34,49 @@ func TestBuildURI(t *testing.T) {
 	actual := c.buildURI("/v2/api")
 	if actual != expected {
 		t.Fatalf("Expected: %s\n But: %s", expected, actual)
+	}
+}
+
+func TestGetBase(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			t.Fatalf("Expected method %s; got %q", http.MethodGet, r.Method)
+		}
+		fmt.Fprintln(w, "{}")
+	}))
+	defer ts.Close()
+
+	t.Log(ts.URL)
+	tc := &config.Config{
+		APIHost: ts.URL,
+	}
+	SetConfig(tc)
+
+	values := url.Values{}
+	err := Get("/testget", values, nil)
+	if err != nil {
+		t.Fatalf("Error on Get Request: %#v", err)
+	}
+}
+
+func TestPostBase(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			t.Fatalf("Expected method %s; got %q", http.MethodPost, r.Method)
+		}
+		fmt.Fprintln(w, "{}")
+	}))
+	defer ts.Close()
+
+	t.Log(ts.URL)
+	tc := &config.Config{
+		APIHost: ts.URL,
+	}
+	SetConfig(tc)
+
+	values := url.Values{}
+	err := Post("/testpost", values, nil)
+	if err != nil {
+		t.Fatalf("Error on Get Request: %#v", err)
 	}
 }
