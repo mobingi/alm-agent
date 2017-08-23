fc5edc50f361dec20d1671a5fe9acd7e4c9ebb71
sawanoboly <sawanoboriyu@higanworks.com>
2017-08-23 18:30:42 +0900

    use versionPath instead

--- server_config/server_config.go
+++ server_config/server_config.go
@@ -29,15 +29,15 @@ type Config struct {
 	EnvironmentVariables map[string]string
 }
 
-var path = "/opt/mobingi/etc/updated"
+var versionPath = "/opt/mobingi/etc/lastUpdated"
 
 // NeedsUpdate checks latest serverconfig
 func NeedsUpdate(c *Config) (bool, error) {
-	if !util.FileExists(path) {
+	if !util.FileExists(versionPath) {
 		return true, nil
 	}
 
-	dat, err := ioutil.ReadFile(path)
+	dat, err := ioutil.ReadFile(versionPath)
 	if err != nil {
 		return false, err
 	}
@@ -45,7 +45,7 @@ func NeedsUpdate(c *Config) (bool, error) {
 	fu := strings.Trim(string(dat), "\n")
 	cu := fmt.Sprintf("%d", c.Updated)
 
-	log.Debugf("updated of %s is %s", path, fu)
+	log.Debugf("updated of %s is %s", versionPath, fu)
 	log.Debugf("updated of serverconfig is %s", cu)
 
 	if fu != cu {
@@ -59,8 +59,8 @@ func NeedsUpdate(c *Config) (bool, error) {
 // WriteUpdated stores current
 func WriteUpdated(c *Config) error {
 	v := fmt.Sprintf("%d", c.Updated)
-	log.Debugf("Write %s to %s", v, path)
-	err := ioutil.WriteFile(path, []byte(v), 0644)
+	log.Debugf("Write %s to %s", v, versionPath)
+	err := ioutil.WriteFile(versionPath, []byte(v), 0644)
 	if err != nil {
 		return err
 	}
