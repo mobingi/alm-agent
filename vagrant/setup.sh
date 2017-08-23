8febfd0e464f55aff16e598f4c8d1ddbf74eeae4
sawanoboly <sawanoboriyu@higanworks.com>
2017-08-23 18:36:28 +0900

    use -f even when setup

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -3,4 +3,4 @@
 mkdir -p /opt/mobingi/alm-agent /opt/mobingi/etc
 wget https://download.labs.mobingi.com/alm-agent/develop/current/alm-agent.tgz
 tar xvzf alm-agent.tgz -C /opt/mobingi/alm-agent
-ln -s /opt/mobingi/alm-agent/v* /opt/mobingi/alm-agent/current
+ln -sf /opt/mobingi/alm-agent/v* /opt/mobingi/alm-agent/current
