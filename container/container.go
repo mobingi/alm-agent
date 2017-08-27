33acd7498697228f88c1796ba9d052ddc9adcd61
sawanoboly <sawanoboriyu@higanworks.com>
2017-08-27 14:41:29 +0900

    create new struct State

--- container/container.go
+++ container/container.go
@@ -7,4 +7,28 @@ type Container struct {
 	Name string
 	ID   string
 	IP   net.IP
+	*State
+}
+
+// State is state of Container from Docker API.
+/*
+  "State": {
+    "Status": "exited",
+    "Running": false,
+    "Paused": false,
+    "Restarting": false,
+    "OOMKilled": false,
+    "Dead": false,
+    "Pid": 0,
+    "ExitCode": 0,
+    "Error": "",
+    "StartedAt": "2017-08-27T05:34:21.077356196Z",
+    "FinishedAt": "2017-08-27T05:34:22.564500324Z"
+	},
+*/
+type State struct {
+	Status   string
+	Running  bool
+	ExitCode int
+	Error    string
 }
