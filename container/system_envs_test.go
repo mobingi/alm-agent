e9ccec5bc781a2a0eea222de47cbe468b1911cb8
sawanoboly <sawanoboriyu@higanworks.com>
2017-08-27 14:12:03 +0900

    add system_env_test

--- container/system_envs_test.go
+++ container/system_envs_test.go
@@ -5,10 +5,21 @@ import (
 
 	"github.com/mobingi/alm-agent/config"
 	"github.com/stretchr/testify/assert"
+	"github.com/mobingi/alm-agent/metavars"
 )
 
 func TestStackID(t *testing.T) {
+	tc := &config.Config{
+		StackID: "teststack"
+	}
+	assert := assert.New(t)
+	assert.Equal("STACK_ID=teststack", envFuncs.StackID(tc))
+}
+
+func InstanceID(t *testing.T) {
+	metavars.ServerID = "dummyid"
+	defer metavars.ServerID = ""
 	tc := &config.Config{}
 	assert := assert.New(t)
-	assert.Equal("STACK_ID=", envFuncs.StackID(tc))
+	assert.Equal("INSTANCE_ID=dummyid", envFuncs.InstanceID(tc))
 }
