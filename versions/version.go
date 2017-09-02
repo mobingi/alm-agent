3acb1da0427ece947d262a810dff6088e234e841
sawanoboly <sawanoboriyu@higanworks.com>
2017-09-02 15:39:55 +0900

    set netlink ver

--- versions/version.go
+++ versions/version.go
@@ -27,10 +27,10 @@ type GoLatest struct {
 }
 
 var (
-	// Version
+	// Version is app version
 	//  - build on CI: majour.minor.epochtime
 	//  - build localy: below
-	Version = "0.2" + "0-dev"
+	Version = "0.3" + "0-dev"
 	// Revision : Commit SHA1.
 	Revision = "local-build"
 	// URLBase : host version_info and binaries.
