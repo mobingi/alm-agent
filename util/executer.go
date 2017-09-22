2e9b0b35f5008a38524a1eb37688ed3d11f7b8ba
sawanoboly <sawanoboriyu@higanworks.com>
2017-09-22 16:46:08 +0900

    define ExecOpts

--- util/executer.go
+++ util/executer.go
@@ -8,7 +8,13 @@ import (
 // ExecuterInterface has Exec to wrap os commands.
 type ExecuterInterface interface {
 	Exec(cmd string, args ...string) ([]byte, error)
-	ExecWithOpts(workdir string, env []string, cmd string, args ...string) ([]byte, error)
+	ExecWithOpts(execopts *ExecOpts, cmd string, args ...string) ([]byte, error)
+}
+
+// ExecOpts is options for ExecWithOpts
+type ExecOpts struct {
+	Dir string
+	Env []string
 }
 
 type osExecuter struct{}
@@ -26,10 +32,10 @@ func (o *osExecuter) Exec(command string, args ...string) ([]byte, error) {
 	return out, err
 }
 
-func (o *osExecuter) ExecWithOpts(dir string, env []string, command string, args ...string) ([]byte, error) {
+func (o *osExecuter) ExecWithOpts(opts *ExecOpts, command string, args ...string) ([]byte, error) {
 	cmd := exec.Command(command, args...)
-	cmd.Dir = dir
-	cmd.Env = env
+	cmd.Dir = opts.Dir
+	cmd.Env = opts.Env
 	out, err := cmd.CombinedOutput()
 	return out, err
 }
@@ -49,11 +55,11 @@ func (m *MockExecuter) Exec(command string, args ...string) ([]byte, error) {
 }
 
 // ExecWithOpts returns command + args and put these to StdOut.
-func (m *MockExecuter) ExecWithOpts(dir string, env []string, command string, args ...string) ([]byte, error) {
+func (m *MockExecuter) ExecWithOpts(opts *ExecOpts, command string, args ...string) ([]byte, error) {
 	cl := command + " " + strings.Join(args, " ")
 	MockBuffer = append(MockBuffer, cl)
-	MockBuffer = append(MockBuffer, dir)
-	MockBuffer = append(MockBuffer, strings.Join(env, ","))
+	MockBuffer = append(MockBuffer, opts.Dir)
+	MockBuffer = append(MockBuffer, strings.Join(opts.Env, ","))
 	out := []byte(cl)
 	return out, nil
 }
