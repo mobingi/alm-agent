edd52658d346d1427244e5d6593e4afe67a1cff8
sawanoboly <sawanoboriyu@higanworks.com>
2017-09-26 23:32:58 +0900

    TestMackerelEnvs

--- container/system_envs_test.go
+++ container/system_envs_test.go
@@ -1,6 +1,7 @@
 package container
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/mobingi/alm-agent/config"
@@ -22,4 +23,25 @@ func TestInstanceID(t *testing.T) {
 	tc := &config.Config{}
 	assert := assert.New(t)
 	assert.Equal([]string{"INSTANCE_ID=dummyid"}, envFuncs.InstanceID(tc, nil))
+}
+
+func TestMackerelEnvs(t *testing.T) {
+	defer func() { metavars.ServerID = "" }()
+
+	assert := assert.New(t)
+	tc := &config.Config{}
+	tc.StackID = "testStack"
+	tc.Flag = "testFlag"
+
+	expect := []string{
+		"apikey=dummykey",
+		"auto_retirement=0",
+		"enable_docker_plugin=1",
+		"opts=-role testStack:testFlag",
+	}
+
+	js := `{"name": "mackarel", "apiKey": "dummykey"}`
+	var opts interface{}
+	json.Unmarshal([]byte(js), &opts)
+	assert.Equal(expect, envFuncs.MackerelEnvs(tc, opts))
 }
