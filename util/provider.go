74d7e99720bfb96eb9cd47d0c8da6ceef8e7e6cf
sawanoboly <sawanoboriyu@higanworks.com>
2018-02-09 14:51:03 +0900

    roughly implements for util.providers

---
+++ util/provider.go
@@ -0,0 +1,77 @@
+package util
+
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// Provider is common interface for cloud services
+type Provider interface {
+	MetadataEndpoint() string
+	GetServerID() (string, error)
+}
+
+type provider struct{}
+
+func (p *provider) simpleHTTPGet(endpoint string) (string, error) {
+	timeout := time.Duration(5 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+	req, _ := http.NewRequest("GET", endpoint, nil)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Warnf("%#v", err)
+		return "", errors.New("Failed to get ServerID")
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Warnf("%#v", err)
+		return "", errors.New("Failed to get ServerID")
+	}
+
+	return string(body), nil
+}
+
+func (p *provider) simpleHTTPGetWithHeader(endpoint string, header []string) (string, error) {
+	timeout := time.Duration(5 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+	req, _ := http.NewRequest("GET", endpoint, nil)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Warnf("%#v", err)
+		return "", errors.New("Failed to get ServerID")
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Warnf("%#v", err)
+		return "", errors.New("Failed to get ServerID")
+	}
+
+	return string(body), nil
+}
+
+func newProvider(name string) (Provider, error) {
+	switch name {
+	case "aws":
+		return &awsProvider{}, nil
+	case "localtest":
+		return &nullProvider{}, nil
+	}
+
+	return &nullProvider{}, errors.New("Provider `" + name + "` is not supported.")
+}
