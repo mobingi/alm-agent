4341018cfb366f1a867836af44ed2602c5ebe478
sawanoboly <sawanoboriyu@higanworks.com>
2018-10-23 15:08:26 +0900

    sharedvolume.Interface

--- shared_volume/shared_volume.go
+++ shared_volume/shared_volume.go
@@ -1,9 +1,29 @@
 package sharedvolume
 
-// SharedVolume mounts on app container
-type SharedVolume struct {
-	Type      string `json:"type"`
-	Identifer string `json:"id"`
-	MountPath string `json:"mount_path"`
-	Options   string `json:"opts"`
+import (
+	"context"
+
+	client "docker.io/go-docker"
+	"docker.io/go-docker/api/types"
+	volumetypes "docker.io/go-docker/api/types/volume"
+)
+
+type Interface interface {
+	CreateVol() (*types.Volume, error)
+}
+
+type LocalVolume struct {
+	client *client.Client
+	name   string
+}
+
+func (v *LocalVolume) CreateVol() (*types.Volume, error) {
+	vol, err := v.client.VolumeCreate(
+		context.Background(),
+		volumetypes.VolumesCreateBody{Name: v.name},
+	)
+	if err != nil {
+		panic(err)
+	}
+	return &vol, nil
 }
