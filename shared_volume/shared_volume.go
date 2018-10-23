b712510ed7d18289355ad1e57ea4abd601eaca56
sawanoboly <sawanoboriyu@higanworks.com>
2018-10-23 17:18:43 +0900

    status()

--- shared_volume/shared_volume.go
+++ shared_volume/shared_volume.go
@@ -2,22 +2,40 @@ package sharedvolume
 
 import (
 	"context"
+	"fmt"
 
 	client "docker.io/go-docker"
 	"docker.io/go-docker/api/types"
+	"docker.io/go-docker/api/types/filters"
 	volumetypes "docker.io/go-docker/api/types/volume"
 )
 
+// Interface is wrapper for docker volume
 type Interface interface {
-	CreateVol() (*types.Volume, error)
+	Setup() (*types.Volume, error)
+	status() *types.Volume
 }
 
+// LocalVolume is simple volume
+// inspect:
+// [
+//     {
+//         "CreatedAt": "2018-10-23T07:36:23Z",
+//         "Driver": "local",
+//         "Labels": {},
+//         "Mountpoint": "/var/lib/docker/volumes/test2/_data",
+//         "Name": "test2",
+//         "Options": {},
+//         "Scope": "local"
+//     }
+// ]
 type LocalVolume struct {
 	client *client.Client
 	name   string
 }
 
-func (v *LocalVolume) CreateVol() (*types.Volume, error) {
+// Setup creates new or return exists volume
+func (v *LocalVolume) Setup() (*types.Volume, error) {
 	vol, err := v.client.VolumeCreate(
 		context.Background(),
 		volumetypes.VolumesCreateBody{Name: v.name},
@@ -26,4 +44,24 @@ func (v *LocalVolume) CreateVol() (*types.Volume, error) {
 		panic(err)
 	}
 	return &vol, nil
+}
+
+func (v *LocalVolume) status() *types.Volume {
+	fmt.Printf("%#v\n", v.name)
+	args := filters.NewArgs(
+		filters.KeyValuePair{
+			Key:   "name",
+			Value: v.name,
+		},
+	)
+	vols, _ := v.client.VolumeList(
+		context.Background(),
+		args,
+	)
+	if len(vols.Volumes) > 0 {
+		fmt.Printf("%#v", vols.Volumes[0])
+		return vols.Volumes[0]
+	}
+
+	return nil
 }
